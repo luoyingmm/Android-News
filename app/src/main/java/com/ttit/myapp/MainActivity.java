a743a1a4fbcbfb2a764abbfe2a1ede4af4f09036
luoyingmm <1502972236@qq.com>
2021-07-20 23:33:50 +0800

    First

--- app/src/main/java/com/ttit/myapp/MainActivity.java
+++ app/src/main/java/com/ttit/myapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.ttit.myapp;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.ttit.myapp.activity.BaseActivity;
 import com.ttit.myapp.activity.LoginActivity;
@@ -9,6 +10,7 @@ import com.ttit.myapp.activity.RegisterActivity;
 
 public class MainActivity extends BaseActivity {
     private Button btn_login,btn_register;
+    private TextView CoverName;
     @Override
     protected int initLayout() {
         return R.layout.activity_main;
@@ -18,6 +20,7 @@ public class MainActivity extends BaseActivity {
     protected void initView() {
         btn_login = findViewById(R.id.btn_login);
         btn_register = findViewById(R.id.btn_register);
+        CoverName = findViewById(R.id.coverName);
     }
 
     @Override
